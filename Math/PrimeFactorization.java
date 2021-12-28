0ff6db17ac55b501b473c4aa4dc0fd9186498ec8
Yuxiang-prog <85142336+Yuxiang-prog@users.noreply.github.com>
2021-12-28 16:14:40 -0500

    Create PrimeFactorization.java

---
+++ Math/PrimeFactorization.java
@@ -0,0 +1,11 @@
+static ArrayList<Integer> factor(int n) {
+	ArrayList<Integer> factors = new ArrayList<>();
+	for (int i = 2; i * i <= n; i++) {
+		while (n % i == 0) {
+			factors.add(i);
+			n /= i;
+		}
+	}
+	if (n > 1) factors.add(n);
+	return factors;
+}
