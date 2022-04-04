6a1b43741577bdff4c8d8c37f55386d00ac42001
Yuxiang-prog <85142336+Yuxiang-prog@users.noreply.github.com>
2022-04-04 16:26:45 -0400

    Create Template.cpp

---
+++ Template.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using ll = long long;
+using namespace std;
+
+void setIO(string s) {
+  freopen((s + ".in").c_str(), "r", stdin);
+  freopen((s + ".out").c_str(), "w", stdout);
+}
+
+int main(){
+  setIO("");
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  
+  return 0;
+}
+
