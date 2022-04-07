6759a1f5c2be426d6ee85c0b2d673775bf7d9824
Yuxiang-prog <85142336+Yuxiang-prog@users.noreply.github.com>
2022-04-06 20:04:54 -0400

    Update Template.cpp

--- Template.cpp
+++ Template.cpp
@@ -2,6 +2,21 @@
 using ll = long long;
 using namespace std;
 
+/* For Rectangle Geometry */
+struct Rect{
+    ll x1, y1, x2, y2;
+    ll area(){
+        return (y2 - y1) * (x2 - x1);
+    }
+};
+
+ll intersect(Rect p, Rect q) {
+    ll xOverlap = max(0LL, min(p.x2, q.x2) - max(p.x1, q.x1));
+    ll yOverlap = max(0LL, min(p.y2, q.y2) - max(p.y1, q.y1));
+    return xOverlap * yOverlap;
+}
+
+
 void setIO(string s) {
   freopen((s + ".in").c_str(), "r", stdin);
   freopen((s + ".out").c_str(), "w", stdout);
